6ec2d142bbb49c24b37a363e2e232751b36f31d0
Estuardo Castro <cas23890@uvg.edu.gt>
2025-04-24 00:28:02 -0600

    2 GETs

--- routes/incidents.js
+++ routes/incidents.js
@@ -16,4 +16,26 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Listar Incidentes
+router.get('/', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM incidents');
+      res.json(result.rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+  
+  // Obtener por ID
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const result = await pool.query('SELECT * FROM incidents WHERE id = $1', [id]);
+      if (result.rows.length === 0) return res.status(404).json({ error: 'Incidente no encontrado' });
+      res.json(result.rows[0]);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+  
 module.exports = router;
